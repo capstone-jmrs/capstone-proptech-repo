f71ea26ace855c6e5799b9f66e8ccb25f4624e1b
Julia Rupp <julia.rupp1987+github@gmail.com>
2022-10-04 13:10:37 +0200

    update eda

--- spotahome_julia_clean.sql
+++ spotahome_julia_clean.sql
@@ -22,18 +22,19 @@ ALTER TABLE spotahome_merged
     ALTER COLUMN platform_id TYPE INT USING platform_id::INT,
     ALTER COLUMN platform TYPE VARCHAR,
     ALTER COLUMN neighborhood TYPE VARCHAR,
-    ALTER COLUMN property_type TYPE VARCHAR,
     ALTER COLUMN housing_type TYPE VARCHAR,
-    --ALTER COLUMN price_pcm TYPE INT USING price_pcm::INT,
+    ALTER COLUMN property_type TYPE VARCHAR,
     ALTER COLUMN title TYPE VARCHAR,
     ALTER COLUMN furnished TYPE VARCHAR,
+    ALTER COLUMN let_type TYPE VARCHAR,
+    --available_today
+    ALTER COLUMN scraping_date TYPE DATE USING scraping_date::DATE,
     ALTER COLUMN price_pcm_0 TYPE INT USING price_pcm_0::INT,
     ALTER COLUMN price_pcm_1 TYPE INT USING price_pcm_1::INT,
-    ALTER COLUMN available_from_total TYPE DATE USING available_from_total::DATE,
-    ALTER COLUMN id TYPE INT USING id::INT,
     ALTER COLUMN bathrooms TYPE INT USING bathrooms::INT,
     ALTER COLUMN m2 TYPE INT USING m2::INT,
-    ALTER COLUMN bedrooms TYPE INT USING bedrooms::INT;
+    ALTER COLUMN bedrooms TYPE INT USING bedrooms::INT,
+    ALTER COLUMN available_from_total TYPE DATE USING available_from_total::DATE;
    
    
 --------------------------------------------------
@@ -43,8 +44,8 @@ ALTER TABLE spotahome_merged
   DROP COLUMN available_from_1,
   DROP COLUMN available_from_2,
   DROP COLUMN id;
-  --DROP COLUMN property_type;
 
+ 
 --------------------------------------------------
 UPDATE spotahome_merged
 SET m2 = NULL
@@ -64,27 +65,36 @@ ALTER TABLE spotahome_merged
 ALTER TABLE spotahome_merged
 	RENAME COLUMN m2 TO size_sqm;
 
---ALTER TABLE spotahome_final_3
-	--RENAME COLUMN housing_type TO property_type;
-
 
 --------------------------------------------------
 UPDATE spotahome_merged
 SET bedrooms = COALESCE(bedrooms, '0')
-WHERE property_type = 'studios';
+WHERE housing_type = 'studios';
 
 UPDATE spotahome_merged
 SET bedrooms = COALESCE(bedrooms, '1')
-WHERE property_type = 'apartments/bedrooms:1';
+WHERE housing_type = 'apartments/bedrooms:1';
 
 UPDATE spotahome_merged
 SET bedrooms = COALESCE(bedrooms, '2')
-WHERE property_type = 'apartments/bedrooms:2';
+WHERE housing_type = 'apartments/bedrooms:2';
 
 UPDATE spotahome_merged
 SET bedrooms = COALESCE(bedrooms, '3')
-WHERE property_type = 'apartments/bedrooms:3';
+WHERE housing_type = 'apartments/bedrooms:3';
+
+UPDATE spotahome_merged
+SET bedrooms = COALESCE(bedrooms, '4')
+WHERE housing_type = 'apartments/bedrooms:3more';
+
+ALTER TABLE spotahome_merged
+	DROP COLUMN housing_type;
 
 
 --------------------------------------------------
---DROP TABLE capstone_jmrs.spotahome_merged;
+SELECT *
+FROM spotahome_merged
+WHERE bedrooms IS NULL;
+
+--------------------------------------------------
+--DROP TABLE spotahome_merged;
